b9b90fa53c24b34d0581731ce6e40e2843795d31
BineethGlobussoft <bineeth@globussoft.in>
2024-06-17 13:01:28 +0000

    first commit

---
+++ calculate.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operation = $_POST['operation'];
+
+    switch ($operation) {
+        case "add":
+            $result = $num1 + $num2;
+            break;
+        case "subtract":
+            $result = $num1 - $num2;
+            break;
+        case "multiply":
+            $result = $num1 * $num2;
+            break;
+        case "divide":
+            if ($num2 != 0) {
+                $result = $num1 / $num2;
+            } else {
+                $result = "Error: Division by zero.";
+            }
+            break;
+        default:
+            $result = "Invalid operation.";
+            break;
+    }
+
+    echo "The result is: " . $result;
+}
+?>
+
